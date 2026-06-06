8e8801eb02cb8fae6e765e929150a8b4745fe569
rocket-fuel86 <NasaSpace65@gmail.com>
2026-06-06 12:12:01 +0300

    Fix staticfiles

--- HW2/settings.py
+++ HW2/settings.py
@@ -128,10 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 if not DEBUG:
     SECURE_SSL_REDIRECT = True
